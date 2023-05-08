2520cc7c59adcb609697d6ac440bfe739282c011
David Craddock <contact@davidcraddock.net>
2023-05-08 23:36:07 +0100

    Fixed mistake - 480p / 2 = 240p

--- create-gif.bat
+++ create-gif.bat
@@ -3,4 +3,4 @@ set arg1=%1
 set arg2=%2
 shift
 shift
-C:\Users\conta\Downloads\ffmpeg-6.0-full_build\bin\ffmpeg.exe -i %arg1% -filter_complex "[0:v] fps=12,scale=280:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" %arg2%
+C:\Users\conta\Downloads\ffmpeg-6.0-full_build\bin\ffmpeg.exe -i %arg1% -filter_complex "[0:v] fps=12,scale=240:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse" %arg2%
